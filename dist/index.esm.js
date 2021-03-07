89be969427bff5254f50c32bf7723912842246f8
DarthViderFinance <darthviderfinance@protonmail.com>
2021-03-07 05:03:45 +0100

    update

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -1991,11 +1991,11 @@ var links = [
         items: [
             {
                 label: "Exchange",
-                href: "https://exchange.tropico.finance",
+                href: "https://exchange.darthvader.finance",
             },
             {
                 label: "Liquidity",
-                href: "https://exchange.tropico.finance/#/pool",
+                href: "https://exchange.darthvader.finance/#/pool",
             },
         ],
     },
@@ -2080,15 +2080,15 @@ var links = [
             // },
             {
                 label: "Github",
-                href: "https://github.com/TropicoFinance",
+                href: "https://github.com/DarthVaderFinance",
             },
             {
                 label: "Docs",
-                href: "https://tropicofinance.medium.com",
+                href: "https://darthvaderfinance.medium.com",
             },
             {
                 label: "Blog",
-                href: "https://tropicofinance.medium.com/",
+                href: "https://darthvaderfinance.medium.com/",
             },
         ],
     },
@@ -2100,7 +2100,7 @@ var socials = [
         items: [
             {
                 label: "English",
-                href: "https://t.me/TropicoFinance",
+                href: "https://t.me/darthvaderFinance",
             },
             // {
             //   label: "Bahasa Indonesia",
@@ -2151,7 +2151,7 @@ var socials = [
     {
         label: "Twitter",
         icon: "TwitterIcon",
-        href: "https://twitter.com/FinanceTropico",
+        href: "https://twitter.com/DarthVaderDefi",
     },
 ];
 var MENU_HEIGHT = 64;
@@ -2735,7 +2735,7 @@ var baseColors = {
     primary: "#e2394f",
     primaryBright: "#e2394f",
     primaryDark: "#e2394f",
-    secondary: "#ffb23d",
+    secondary: "#ee3044",
     success: "#31D0AA",
     warning: "#e2394f",
 };
@@ -2745,7 +2745,7 @@ var brandColors = {
 var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#FAF9FA", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#eeeaf4", tertiary: "#EFF4F5", text: "#ffb23d", textDisabled: "#BDC2C4", textSubtle: "#8f80ba", borderColor: "#E9EAEB", card: "#FFFFFF", gradients: {
         bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
     } });
-var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#ffb23d", background: "#343135", backgroundDisabled: "#3c3742", contrast: "#FFFFFF", invertedContrast: "#191326", input: "#483f5a", primaryDark: "#0098A1", tertiary: "#353547", text: "#EAE2FC", textDisabled: "#666171", textSubtle: "#c9c4d4", borderColor: "#524B63", card: "#27262c", gradients: {
+var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#e7e7e7", background: "#101010", backgroundDisabled: "#3c3742", contrast: "#FFFFFF", invertedContrast: "#191326", input: "#483f5a", primaryDark: "#0098A1", tertiary: "#353547", text: "#EAE2FC", textDisabled: "#666171", textSubtle: "#c9c4d4", borderColor: "#524B63", card: "#27262c", gradients: {
         bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
     } });
 
